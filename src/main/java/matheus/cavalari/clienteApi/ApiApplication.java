7e733a52ea904e6f3bde2a46953bcd3a0f45367a
Matheus Cavalari <matheuscavbarbosa@hotmail.com>
2024-05-31 10:37:46 -0300

    Criação do projeto

--- src/main/java/matheus/cavalari/clienteApi/ApiApplication.java
+++ src/main/java/matheus/cavalari/clienteApi/ApiApplication.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class ApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
